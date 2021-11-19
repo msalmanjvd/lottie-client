a733b1d037d7a80a76d645aada333fd7e90b4667
Salman Javed <msalmanjvd@gmail.com>
2021-11-20 01:15:42 +0500

    tailwind changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-import tailwind from "tailwindcss";
-import autoprefixer from "autoprefixer";
-import postcssimport from "postcss-import";
 module.exports = {
   plugins: {
-    postcssimport,
-    tailwindcss: { tailwind },
-    autoprefixer: { autoprefixer },
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
