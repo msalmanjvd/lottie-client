1faaabdb8c6a59197e387f0a1239e767ae6cbeb7
Salman Javed <msalmanjvd@gmail.com>
2021-11-20 02:03:01 +0500

    tailwind changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,12 +7,8 @@ const Navbar: FC = () => {
   const src = "https://static.lottiefiles.com/images/v3/lottiefiles-logo.svg";
 
   return (
-    <nav className=" bg-gray-200 fixed z-10 mt-16 h-16 left-0 w-full  mt-16 ">
-      <div className=" flex justify-center sm:justify-start items-center h-full w-full  md:p-0 md:p-0">
-        {/* <div className="pt-5">
-          <Image loader={() => src} src={src} width={150} height={30} />
-        </div> */}
-
+    <nav className="bg-gray-200 fixed z-10 mt-16 h-16 left-0 w-full  mt-16 ">
+      <div className="flex justify-center sm:justify-start items-center h-full w-full  md:p-0 md:p-0">
         <div className="w-full flex flex-row  justify-center text-center">
           {NavButtons.map((nav) => (
             <>
