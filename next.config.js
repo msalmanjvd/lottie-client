d5f2214ef4c8fc08c8e51f428c8706c156cfa135
Salman Javed <msalmanjvd@gmail.com>
2021-11-19 21:10:51 +0500

    add node_mdoeuls to gitignore

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   reactStrictMode: true,
   graphql: "http://localhost:6060/grapql",
   file_server: "http://localhost:4000",
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
