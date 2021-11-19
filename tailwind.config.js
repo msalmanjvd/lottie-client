064f95ffb110a847b7f8126599ea2bcfbf0062de
Salman Javed <msalmanjvd@gmail.com>
2021-11-20 02:41:18 +0500

    tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,11 @@ module.exports = {
     // colors: {
     //   main: "#0FCCCE",
     // },
-    textColor: (theme) => theme("colors"),
-    textColor: {
-      main: "#0FCCCE",
+    extend: {
+      colors: {
+        main: "#0FCCCE",
+      },
     },
-    backgroundColor: (theme) => ({
-      ...theme("colors"),
-      main: "#0FCCCE",
-    }),
-    extend: {},
   },
   variants: {
     extend: {},
