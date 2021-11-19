4c7b3cf490d75ea0d84e2907e414e473d82ffc54
Salman Javed <msalmanjvd@gmail.com>
2021-11-20 02:07:55 +0500

    tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
   purge: ["./components/**/*.tsx", "./pages/**/*.tsx", "./public/**/*.html"], //add this line
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      main: "#0FCCCE",
-    },
+    // colors: {
+    //   main: "#0FCCCE",
+    // },
     extend: {},
   },
   variants: {
